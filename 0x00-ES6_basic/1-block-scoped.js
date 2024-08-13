edcad64dab75ced7fa5821bd84ada4c14df45822
Sheelah <sheelah.mogaka@gmal.com>
2024-08-13 13:51:40 +0000

    1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,7 @@ export default function taskBlock(trueOrFalse) {
   var task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+	return [task, task2]
   }
 
   return [task, task2];
